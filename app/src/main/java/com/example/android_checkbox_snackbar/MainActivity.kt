da4a45a9905c1bd39018823193d4533a94bf5d24
Hellion <semakin.s.v@gmail.com>
2024-10-18 00:35:19 +0300

    app done

--- app/src/main/java/com/example/android_checkbox_snackbar/MainActivity.kt
+++ app/src/main/java/com/example/android_checkbox_snackbar/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.android_checkbox_snackbar
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var editTextET: EditText
+    private lateinit var textViewTV: TextView
+
+    private lateinit var saveDataBTN: Button
+    private lateinit var deleteDataBTN: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +30,35 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        editTextET = findViewById(R.id.editTextET)
+        textViewTV = findViewById(R.id.textViewTV)
+
+        saveDataBTN = findViewById(R.id.saveDataBTN)
+        deleteDataBTN = findViewById(R.id.deleteDataBTN)
+
+        saveDataBTN.setOnClickListener(this)
+        deleteDataBTN.setOnClickListener(this)
     }
+
+    override fun onClick(p0: View) {
+        when(p0.id){
+            R.id.saveDataBTN -> {
+                textViewTV.setText(editTextET.text)
+            }
+
+            R.id.deleteDataBTN -> {
+                Snackbar.make(p0, "Подтвердите удаление", Snackbar.LENGTH_LONG)
+                    .setAction("Удалить") {
+                        editTextET.text.clear()
+                        textViewTV.setText("")
+                        Snackbar.make(p0, "Данные удалены", Snackbar.LENGTH_LONG).show()
+                    }
+                    .setActionTextColor(Color.BLUE)
+                    .show()
+            }
+        }
+    }
+
+
 }
